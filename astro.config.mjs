daadb372f61d1172c8b1efa43165a755985a3760
Tekhion <myshlakovalexey@gmail.com>
2025-08-03 23:07:53 +0200

    Deploy Astro site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+  ],
+  site: `https://tekhion.com`,
+  output: "static",          // Esto para que genere archivos estáticos
+  outDir: "./docs",          // Carpeta donde se generarán los archivos para GitHub Pages
+  build: {
+    assets: "astro",         // Elimina el guion bajo si lo tenías antes
+  },
+});
