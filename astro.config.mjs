c2868685c438733e46a34ca21077c0ac74aad879
Tekhion <myshlakovalexey@gmail.com>
2025-08-03 23:40:18 +0200

    .js fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ export default defineConfig({
   integrations: [
   ],
   site: `https://tekhion.com`,
-  output: "static",          // Esto para que genere archivos estáticos
-  outDir: "./docs",          // Carpeta donde se generarán los archivos para GitHub Pages
+  output: "static",
+  outDir: "./docs",
   build: {
-    assets: "astro",         // Elimina el guion bajo si lo tenías antes
+  assets: "astro",
   },
 });
