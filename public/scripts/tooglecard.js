f86728ebbc081a01b9f0f823591d0cd2265af48a
Tekhion <myshlakovalexey@gmail.com>
2025-08-04 23:44:28 +0200

    4.8.25 Update + Added Project Information

--- public/scripts/tooglecard.js
+++ public/scripts/tooglecard.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('modal');
   const modalImage = document.getElementById('modal-image');
@@ -33,4 +36,19 @@ document.addEventListener('DOMContentLoaded', () => {
       modal.classList.remove('show');
     }
   });
+
+  const helpButton = document.getElementById('help-button');
+  if (helpButton) {
+    helpButton.addEventListener('click', () => {
+      modalImage.src = '/media/ImagesDiv/104.jpg';
+      modalText.innerHTML = `
+        <h2>What is this project?</h2>
+        <p>This build is based on an anime/visual novel I saw a while ago, Steins;gate, which has a function within it, which I'll let you discover on your own if you're interested in watching it (recommended :) ). Apart from that you could also say that it will have double use as a nixie tube clock as the chip itself that allows this costs relatively little and in the original project they show how to install it. <br><br></p>
+        <p>This project is possible thanks to Tom, as well as all the design, PCBs, steps that I do in the assembly... and a lot of things more, which I am very grateful and I invite you to visit <a href="http://brotoro.com/dm/" style="color: orange;">the original project.</a></p>
+      `;
+      modal.classList.add('show');
+      modal.scrollTo(0, 0);
+    });
+  }
+
 });
